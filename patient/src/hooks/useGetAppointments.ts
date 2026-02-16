780cdbf1d2a97322711168407094b5bbab265fa4
Brijbhan Chauhan <brijphy@gmail.com>
2026-02-16 10:46:25 -0800

    log maintained

--- patient/src/hooks/useGetAppointments.ts
+++ patient/src/hooks/useGetAppointments.ts
@@ -58,8 +58,7 @@ export default function useGetAppointments() {
                 ) {
                     upcoming.push(a);
                 } else if (
-                    a.status === "completed" &&
-                    (isPastDate || (isToday && isPastTime))
+                    (a.status === "completed" || a.status === "cancelled")
                 ) {
                     previous.push(a);
                 } else if (
